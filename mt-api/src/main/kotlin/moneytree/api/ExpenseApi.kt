0e709718f211bac303ebf2a405258e395726d9e6
Eric <fruitbraker@users.noreply.github.com>
2021-01-17 09:58:40 -0600

    Add expense summary endpoint (#15)
    Also updated Grade, libraries, and target jvm version.

--- mt-api/src/main/kotlin/moneytree/api/ExpenseApi.kt
+++ mt-api/src/main/kotlin/moneytree/api/ExpenseApi.kt
@@ -1,11 +1,21 @@
 package moneytree.api
 
+import java.util.UUID
 import moneytree.MtApiRoutes
+import moneytree.domain.ExpenseSummaryRepository
 import moneytree.domain.Repository
 import moneytree.domain.entity.Expense
+import moneytree.domain.entity.ExpenseSummary
+import moneytree.processGetByIdResult
+import moneytree.processGetResult
+import moneytree.validator.ValidationResult
 import moneytree.validator.Validator
+import moneytree.validator.validateUUID
 import org.http4k.core.Body
 import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
 import org.http4k.format.Jackson.auto
 import org.http4k.lens.BiDiBodyLens
 import org.http4k.routing.RoutingHttpHandler
@@ -19,15 +29,42 @@ class ExpenseApi(
     repository,
     validator
 ) {
+    private val expenseSummaryRepository = repository as ExpenseSummaryRepository
+
     override val lens: BiDiBodyLens<Expense> = Body.auto<Expense>().toLens()
     override val listLens: BiDiBodyLens<List<Expense>> = Body.auto<List<Expense>>().toLens()
 
+    private val summaryLens: BiDiBodyLens<ExpenseSummary> = Body.auto<ExpenseSummary>().toLens()
+    private val summaryListLens: BiDiBodyLens<List<ExpenseSummary>> = Body.auto<List<ExpenseSummary>>().toLens()
+
     override fun makeRoutes(): RoutingHttpHandler {
         return routes(
+            "/expense/summary" bind Method.GET to this::getExpenseSummary,
+            "/expense/summary/{id}" bind Method.GET to this::getExpenseSummaryById,
             "/expense" bind Method.GET to ::get,
             "/expense/{id}" bind Method.GET to ::getById,
             "/expense" bind Method.POST to ::insert,
             "/expense/{id}" bind Method.PUT to ::upsertById
         )
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun getExpenseSummary(request: Request): Response {
+        return processGetResult(expenseSummaryRepository.getExpenseSummary(), summaryListLens)
+    }
+
+    private fun getExpenseSummaryById(request: Request): Response {
+        val uuid = idLens(request)
+        return when (uuid.validateUUID()) {
+            ValidationResult.Accepted -> processGetByIdResult(
+                expenseSummaryRepository.getExpenseSummaryById(
+                    UUID.fromString(
+                        uuid
+                    )
+                ),
+                summaryLens
+            )
+            ValidationResult.Rejected -> Response(Status.BAD_REQUEST)
+        }
     }
 }
--- mt-api/src/test/kotlin/moneytree/api/ExpenseApiTest.kt
+++ mt-api/src/test/kotlin/moneytree/api/ExpenseApiTest.kt
@@ -6,6 +6,7 @@ import io.mockk.mockkClass
 import java.time.LocalDate
 import java.util.UUID
 import moneytree.domain.entity.Expense
+import moneytree.domain.entity.ExpenseSummary
 import moneytree.libs.commons.result.toOk
 import moneytree.libs.commons.serde.toJson
 import moneytree.libs.http4k.buildRoutes
@@ -32,7 +33,9 @@ class ExpenseApiTest {
     private val todayLocalDate = LocalDate.now()
     private val randomTransactionAmount = randomBigDecimal()
     private val vendorId = UUID.randomUUID()
+    private val vendorName = randomString()
     private val expenseCategoryId = UUID.randomUUID()
+    private val expenseCategory = randomString()
     private val notes = randomString()
     private val hide = false
 
@@ -42,6 +45,18 @@ class ExpenseApiTest {
         transactionDate = todayLocalDate,
         vendor = vendorId,
         expenseCategory = expenseCategoryId,
+        notes = notes,
+        hide = hide
+    )
+
+    private val expenseSummary = ExpenseSummary(
+        id = randomUUID,
+        transactionAmount = randomTransactionAmount,
+        transactionDate = todayLocalDate,
+        vendorId = vendorId,
+        vendorName = vendorName,
+        expenseCategoryId = expenseCategoryId,
+        expenseCategory = expenseCategory,
         notes = notes,
         hide = hide
     )
@@ -63,6 +78,8 @@ class ExpenseApiTest {
         every { expenseRepository.getById(randomUUID) } returns expense.toOk()
         every { expenseRepository.insert(expense) } returns expense.toOk()
         every { expenseRepository.upsertById(expense, randomUUID) } returns expense.toOk()
+        every { expenseRepository.getExpenseSummary() } returns listOf(expenseSummary).toOk()
+        every { expenseRepository.getExpenseSummaryById(randomUUID) } returns expenseSummary.toOk()
 
         server.start()
         return "http://localhost:${server.port()}/expense"
@@ -124,5 +141,31 @@ class ExpenseApiTest {
 
         result.status shouldBe Status.OK
         result.bodyString() shouldBe expense.toJson()
+    }
+
+    @Test
+    fun `getExpenseSummary happy path`() {
+        val request = Request(
+            Method.GET,
+            "$url/summary"
+        )
+
+        val result = client(request)
+
+        result.status shouldBe Status.OK
+        result.bodyString() shouldBe listOf(expenseSummary).toJson()
+    }
+
+    @Test
+    fun `getExpenseSummaryById happy path`() {
+        val request = Request(
+            Method.GET,
+            "$url/summary/$randomUUID"
+        )
+
+        val result = client(request)
+
+        result.status shouldBe Status.OK
+        result.bodyString() shouldBe expenseSummary.toJson()
     }
 }
