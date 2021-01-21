feb458093d52fd442d73f9060d52a4be7c5533cf
fruitbraker <congpeng89@gmail.com>
2021-01-20 20:51:06 -0600

    Added tests to ResultProcessor for the upsert operations.

--- mt-api/src/test/kotlin/moneytree/api/ResultProcessorTest.kt
+++ mt-api/src/test/kotlin/moneytree/api/ResultProcessorTest.kt
@@ -1,12 +1,15 @@
 package moneytree.api
 
 import io.kotest.matchers.shouldBe
+import moneytree.libs.commons.result.toErr
 import moneytree.libs.commons.result.toOk
 import moneytree.libs.commons.serde.toJson
 import moneytree.libs.testcommons.randomString
+import moneytree.persist.FOREIGN_KEY_CONSTRAINT_VIOLATION
 import moneytree.processGetByIdResult
 import moneytree.processGetResult
 import moneytree.processInsertResult
+import moneytree.processUpsertResult
 import org.http4k.core.Body
 import org.http4k.core.Status
 import org.http4k.format.Jackson.auto
@@ -43,6 +46,15 @@ class ResultProcessorTest {
     }
 
     @Test
+    fun `processGetResult returns bad request on error result`() {
+        val errResult = SampleThrowable().toErr()
+
+        val result = processGetResult(errResult, listLens)
+
+        result.status shouldBe Status.BAD_REQUEST
+    }
+
+    @Test
     fun `processGetByIdResult returns ok with nonnull entity`() {
         val sample = Sample(randomString())
         val okResult = sample.toOk()
@@ -64,6 +76,15 @@ class ResultProcessorTest {
     }
 
     @Test
+    fun `processGetByIdResult returns bad request on error result`() {
+        val errResult = SampleThrowable().toErr()
+
+        val result = processGetByIdResult(errResult, lens)
+
+        result.status shouldBe Status.BAD_REQUEST
+    }
+
+    @Test
     fun `processInsertResult returns created onOk`() {
         val sample = Sample(randomString())
         val okResult = sample.toOk()
@@ -74,7 +95,47 @@ class ResultProcessorTest {
         result.bodyString() shouldBe sample.toJson()
     }
 
+    @Test
+    fun `processInsertResult returns bad request on error result`() {
+        val errResult = SampleThrowable().toErr()
+
+        val result = processInsertResult(errResult, lens)
+
+        result.status shouldBe Status.BAD_REQUEST
+    }
+
+    @Test
+    fun `processUpsertResult returns ok happy path`() {
+        val sample = Sample(randomString())
+        val okResult = sample.toOk()
+
+        val result = processUpsertResult(okResult, lens)
+
+        result.status shouldBe Status.OK
+        result.bodyString() shouldBe sample.toJson()
+    }
+
+    @Test
+    fun `processUpsertResult returns bad request on error result`() {
+        val errResult = SampleThrowable().toErr()
+
+        val result = processUpsertResult(errResult, lens)
+
+        result.status shouldBe Status.BAD_REQUEST
+    }
+
+    @Test
+    fun `processUpsertResult returns conflict on foreign key constraint violation`() {
+        val errResult = SampleThrowable(FOREIGN_KEY_CONSTRAINT_VIOLATION).toErr()
+
+        val result = processUpsertResult(errResult, lens)
+
+        result.status shouldBe Status.CONFLICT
+    }
+
     private data class Sample(
         val blah: String
     )
+
+    private class SampleThrowable(reason: String = randomString()): Throwable(reason)
 }
