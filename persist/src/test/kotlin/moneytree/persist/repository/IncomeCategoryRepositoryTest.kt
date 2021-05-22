b28f8957f500920ef2209586618db6f67beabaf9
fruitbraker <congpeng89@gmail.com>
2021-05-21 19:47:31 -0500

    income category test refactor

--- persist/src/test/kotlin/moneytree/persist/repository/IncomeCategoryRepositoryTest.kt
+++ persist/src/test/kotlin/moneytree/persist/repository/IncomeCategoryRepositoryTest.kt
@@ -7,24 +7,19 @@ import java.util.UUID
 import moneytree.domain.entity.IncomeCategory
 import moneytree.libs.commons.result.onOk
 import moneytree.libs.commons.result.shouldBeOk
+import moneytree.libs.commons.result.toOkValue
 import moneytree.libs.testcommons.randomString
 import moneytree.persist.PersistConnectorTestHarness
-import moneytree.persist.db.generated.tables.daos.IncomeCategoryDao
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class IncomeCategoryRepositoryTest {
-
-    private val persistHarness = PersistConnectorTestHarness()
-    private val incomeCategoryRepository = IncomeCategoryRepository(
-        IncomeCategoryDao(persistHarness.dslContext.configuration())
-    )
+class IncomeCategoryRepositoryTest : PersistConnectorTestHarness() {
 
     @AfterAll
     fun clean() {
-        persistHarness.close()
+        super.close()
     }
 
     @Test
@@ -37,73 +32,48 @@ class IncomeCategoryRepositoryTest {
 
     @Test
     fun `insert and getById happy path`() {
-        val randomUUID = UUID.randomUUID()
-        val randomString = randomString()
+        val randomIncomeCategory = insertRandomIncomeCategory().toOkValue()
 
-        val incomeCategory = IncomeCategory(
-            id = randomUUID,
-            name = randomString
+        val retrieveResult = incomeCategoryRepository.getById(
+            checkNotNull(randomIncomeCategory.id)
         )
-
-        val insertResult = incomeCategoryRepository.insert(incomeCategory)
-        insertResult.shouldBeOk()
-        insertResult.onOk {
-            it shouldBe incomeCategory
-        }
-
-        val retrieveResult = incomeCategoryRepository.getById(randomUUID)
         retrieveResult.shouldBeOk()
         retrieveResult.onOk {
-            it shouldBe incomeCategory
+            it shouldBe randomIncomeCategory
         }
     }
 
     @Test
     fun `generic get returns a list`() {
-        val randomUUID = UUID.randomUUID()
-        val randomString = randomString()
-
-        val incomeCategory = IncomeCategory(
-            id = randomUUID,
-            name = randomString
-        )
-
-        incomeCategoryRepository.insert(incomeCategory)
+        val randomIncomeCategory = insertRandomIncomeCategory().toOkValue()
 
         val getResult = incomeCategoryRepository.get()
         getResult.shouldBeOk()
         getResult.onOk {
             it.size shouldBeGreaterThanOrEqual 1
-            it shouldContain incomeCategory
+            it shouldContain randomIncomeCategory
         }
     }
 
     @Test
     fun `upsert updates existing entity`() {
-        val randomUUID = UUID.randomUUID()
-        val randomString = randomString()
-
-        val incomeCategory = IncomeCategory(
-            id = randomUUID,
-            name = randomString
-        )
-
-        incomeCategoryRepository.insert(incomeCategory)
+        val randomIncomeCategory = insertRandomIncomeCategory().toOkValue()
+        val randomIncomeCategoryId = checkNotNull(randomIncomeCategory.id)
 
         val newRandomString = randomString()
 
         val updatedIncomeCategory = IncomeCategory(
-            id = randomUUID,
+            id = randomIncomeCategoryId,
             name = newRandomString
         )
 
-        val insertResult = incomeCategoryRepository.upsertById(updatedIncomeCategory, randomUUID)
-        insertResult.shouldBeOk()
-        insertResult.onOk {
+        val upsertResult = incomeCategoryRepository.upsertById(updatedIncomeCategory, randomIncomeCategoryId)
+        upsertResult.shouldBeOk()
+        upsertResult.onOk {
             it shouldBe updatedIncomeCategory
         }
 
-        val retrieveResult = incomeCategoryRepository.getById(randomUUID)
+        val retrieveResult = incomeCategoryRepository.getById(randomIncomeCategoryId)
         retrieveResult.shouldBeOk()
         retrieveResult.onOk {
             it shouldBe updatedIncomeCategory
@@ -159,21 +129,14 @@ class IncomeCategoryRepositoryTest {
 
     @Test
     fun `deleteById successfully deletes`() {
-        val randomUUID = UUID.randomUUID()
-        val randomString = randomString()
+        val randomIncomeCategory = insertRandomIncomeCategory().toOkValue()
+        val randomIncomeCategoryId = checkNotNull(randomIncomeCategory.id)
 
-        val incomeCategory = IncomeCategory(
-            id = randomUUID,
-            name = randomString
-        )
 
-        val insertResult = incomeCategoryRepository.insert(incomeCategory)
-        insertResult.shouldBeOk()
-
-        val deleteResult = incomeCategoryRepository.deleteById(randomUUID)
+        val deleteResult = incomeCategoryRepository.deleteById(randomIncomeCategoryId)
         deleteResult.shouldBeOk()
 
-        val nullResult = incomeCategoryRepository.getById(randomUUID)
+        val nullResult = incomeCategoryRepository.getById(randomIncomeCategoryId)
         nullResult.shouldBeOk()
         nullResult.onOk { it shouldBe null }
     }
